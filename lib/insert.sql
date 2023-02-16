5e3dd3b8b7ccc2f1e3e7b4558c6e5f9c769e521f
Derrick Momanyi <derrick.momanyi@student.moringaschool.com>
2023-02-16 23:01:49 +0300

    Done

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Mr. Chocolate", "1", "M", "White", "sanguine", "1");
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Rowdy", "2", "F", "Blue", "choleric", "0");
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Tabitha", "3", "M", "Red", "melancholic", "1");
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Sergeant Brown", "4", "F", "Indigo", "phlegmatic", "0");
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Melissa", "5", "M", "Yellow", "choleric", "1");
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Grinch", "6", "F", "Green", "sanguine", "0");
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Wendy", "7", "M", "Orange", "phlegmatic", "1");
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (NULL, "8", "F", "Purple", "melancholic", "0");
